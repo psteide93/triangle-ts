b07b1778136fcb0a5d6be4b9a2a71e460f9cfc52
Parnell <psteide@ford.com>
2022-05-06 13:59:32 -0400

    Add Triangle class info

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,53 @@
-export class Triangle {}
+export class Triangle {
+  sideOne = 0;
+  sideTwo = 0;
+  sideThree = 0;
+  constructor(sideOne: number, sideTwo: number, sideThree: number) {
+    this.sideOne = sideOne;
+    this.sideTwo = sideTwo;
+    this.sideThree = sideThree;
+  }
+
+  get isEquilateral() {
+    if (
+      this.sideOne != 0 &&
+      this.sideOne === this.sideTwo &&
+      this.sideTwo === this.sideThree
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get isIsosceles() {
+    if (
+      this.sideOne === this.sideTwo ||
+      this.sideTwo === this.sideThree ||
+      this.sideThree === this.sideOne
+    ) {
+      if (
+        this.sideOne > this.sideTwo + this.sideThree ||
+        this.sideThree > this.sideTwo + this.sideOne ||
+        this.sideTwo > this.sideThree + this.sideOne
+      ) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  get isScalene() {
+    if (this.sideOne > this.sideTwo + this.sideThree||
+      this.sideOne === this.sideTwo ||
+      (this.sideOne === this.sideTwo && this.sideTwo === this.sideThree)
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
